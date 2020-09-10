40a68db0a370beb204f1fadb2249991c530cf1ed
Charles Karari <kararicw@yahoo.com>
2020-09-10 22:24:38 +0300

    changes to project structure

--- index.js
+++ index.js
@@ -173,15 +173,15 @@ $(document).ready(function () {
             $("#pizzatotal").hide();
             $(".delivery").hide();
             $("button#final-order").hide();
-            let deliceryamount = checkoutTotal + 200;
-            console.log("Final Bill is: " + deliceryamount);
+            let deliveryamount = checkoutTotal + 200;
+            console.log("Final Bill is: " + deliveryamount);
             let person = $("input#name").val();
             let phone = $("input#phone").val();
             let location = $("input#location").val();
 
             if ($("input#name").val() && $("input#phone").val() && $("input#location").val() != "") {
 
-                $("#finallmessage").append(person + ", We have recieved your order and it will be delivered to you at " + location + ". Prepare sh. " + deliceryamount);
+                $("#finallmessage").append(person + ", We have recieved your order and it will be delivered to you at " + location + ". Prepare sh. " + deliveryamount);
                 $("#totalbill").hide();
                 $("#finallmessage").slideDown(1200);
             }
